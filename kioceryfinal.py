bd46937a4d68ca15ed613d52597bff3fb83207ed
Karl Mart <kmherrera25@gmail.com>
2019-04-23 21:40:43 +0800

    ver 0.2

--- kioceryfinal.py
+++ kioceryfinal.py
@@ -6,8 +6,33 @@ import re
 import mysql.connector
 from mysql.connector import MySQLConnection, Error
 
-def rfidconnect():   # Connect to RFID
+def rfid_connect():   # Connect RFID
     try:
-        
+        baud = 9600
+        ser = serial.Serial('/dev/ttyUSB0', baud, timeout= 0.2)
+        if ser.isOpen():
+            print(ser.name + ' is open...')
+            ser.write(serial.to_bytes([0x7C, 0xFF, 0xFF, 0x82, 0x32, 0x00, 0xD2]))
+            response = binascii.hexlify(ser.readline())
+            response = str(response, 'UTF8')
+            print(response)
+            if response == device_check:
+                print("RFID Properly Initialized")
+    except Error as e:
+        print(e)
+
+def rfid_disconnect(): # Disconnect RFID
+    try:
+        print("Program Closing")
+        time.sleep(2)
+        ser.close()
+        print('closed')
+        break
+    except Error as e:
+        print(e)
+
+def scan(): # Disconnect RFID
+    try:
+        pass
     except Exception as e:
         raise
