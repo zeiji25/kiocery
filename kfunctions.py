29b2bf54756c75f594d3c66c9bba57bf00b3bb6b
Karl Mart <kmherrera25@gmail.com>
2019-04-23 21:47:21 +0800

    ver 0.1

---
+++ kfunctions.py
@@ -0,0 +1,49 @@
+import serial
+import serial.tools.list_ports
+import binascii
+import time
+import re
+import mysql.connector
+from mysql.connector import MySQLConnection, Error
+
+def rfid_connect():   # Connect RFID
+    try:
+        baud = 9600
+        ser = serial.Serial('/dev/ttyUSB0', baud, timeout= 0.2)
+        if ser.isOpen():
+            print(ser.name + ' is open...')
+            ser.write(serial.to_bytes([0x7C, 0xFF, 0xFF, 0x82, 0x32, 0x00, 0xD2]))
+            response = binascii.hexlify(ser.readline())
+            response = str(response, 'UTF8')
+            print(response)
+            if response == device_check:
+                print("RFID Properly Initialized")
+    except Error as e:
+        print(e)
+
+def rfid_disconnect(): # Disconnect RFID
+    try:
+        print("Program Closing")
+        time.sleep(2)
+        ser.close()
+        print('closed')
+        break
+    except Error as e:
+        print(e)
+
+def scan(): # Disconnect RFID
+    try:
+        tags = []
+        for x in range(0, 15, 1):
+            ser.write(serial.to_bytes([0x7C, 0xFF, 0xFF, 0x11, 0x32, 0x00, 0x43]))
+            response = binascii.hexlify(ser.read(500))
+            response = str(response, 'UTF8')
+            for i in range(14, len(response), 28):
+                tags.append(response[i+2:i+26])
+                tags = filter(None, tags)
+                tags = list(set(tags))
+                tags.sort()
+            print(tags)
+        print(len(tags))
+    except Exception as e:
+        raise
