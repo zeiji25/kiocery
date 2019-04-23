0686992a221540b78ab9f176d8fda1bd33fa19b6
Karl Mart <kmherrera25@gmail.com>
2019-04-23 19:13:00 +0800

    Add Kiocery Final Program

---
+++ kioceryfinal.py
@@ -0,0 +1,13 @@
+import serial
+import serial.tools.list_ports
+import binascii
+import time
+import re
+import mysql.connector
+from mysql.connector import MySQLConnection, Error
+
+def rfidconnect():   # Connect to RFID
+    try:
+        
+    except Exception as e:
+        raise
