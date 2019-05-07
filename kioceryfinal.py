30d75f11d86af0166514d1656c5481ad651c2125
Karl Mart <kmherrera25@gmail.com>
2019-05-07 21:44:48 +0800

    asdad

--- kioceryfinal.py
+++ kioceryfinal.py
@@ -20,10 +20,23 @@ from kivy.uix.video import Video
 from kivy.uix.scatter import Scatter
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.uix.progressbar import ProgressBar
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.recycleview import RecycleView
+from kivy.config import Config
+
 Window.maximize()
-tags = []
+totaltags = [1,2,3,4,5,6,7,8]
 response = []
 final = []
+sortedtags = []
+distrib = []
 
 #baud = 9600
 #ser = serial.Serial('COM7', baud, timeout= 0.2)
@@ -36,7 +49,7 @@ final = []
 #    print("RFID Properly Initialized")
 
 def scan(self): # Disconnect RFID
-        global tags, response, final
+        global totaltags, response, final, sortedtags, distrib
         def regs(list):
             regex_string = re.findall(r'(?<=01)30\d{22}(?=b)' , list)
             return regex_string
@@ -45,21 +58,63 @@ def scan(self): # Disconnect RFID
             response = binascii.hexlify(ser.readline())
             response = str(response, 'UTF8')
             reduc = regs(response)
-            tags = list(set(tags).union(set(reduc)))
+            totaltags.extend(reduc)
+            sortedtags.sort(list(set(c).union(set(b))))
+            distrib = [len(list(group)) for key, group in groupby(totatags)]
         print('\n\n')
-        tags.sort()
-        print(tags)
-        print(len(tags))
+        print(len(sortedtags))
+        print(len(distrib))
+
+class MyPopupProgressBar(Widget):
+
+    progress_bar = ObjectProperty() # Kivy properties classes are used when you create an EventDispatcher.
+
+    def __init__(self, **kwa):
+        super(MyPopupProgressBar, self).__init__(**kwa) #super combines and initializes two widgets Popup and ProgressBar
+        self.progress_bar = ProgressBar() # instance of ProgressBar created.
+        self.popup = Popup(title='Scanning', content=self.progress_bar) # progress bar assigned to popup
+        self.popup.bind(on_open=self.puopen) # Binds super widget to on_open.
+        Clock.schedule_once(self.progress_bar_start) # Uses clock to call progress_bar_start() (callback) one time only
+
+    def progress_bar_start(self, instance): # Provides initial value of of progress bar and lanches popup
+        self.progress_bar.value = 1 # Initial value of progress_bar
+        self.popup.size_hint = (.5,.5)
+        self.popup.open() # starts puopen()
+
+    def next(self, dt): # Updates Project Bar
+        if self.progress_bar.value >= 100: # Checks to see if progress_bar.value has met 100
+            self.popup.dismiss()
+            return False # Returning False schedule is canceled and won't repeat
+        self.progress_bar.value += 2 # Updates progress_bar's progress
+
+    def puopen(self, instance): # Called from bind.
+        Clock.schedule_interval(self.next, .0005) # Creates Clock event scheduling next() every 5-1000th of a second.
+
 
 class MainScreen(Screen):
     pass
 
 class POS(Screen):
+    # def __init__(self, **kwargs):
+    #     super(POS, self).__init__(**kwargs)
+    #     # assigning data in RecyclerView
+    #     global totaltags, response, final, sortedtags, distrib
+    #     totaltags = ['1','2','3','4','5','6','7','8']
+    #     self.data = [{'text': totaltags(x)} for x in range(100)]
     pass
+class Receipt(Screen):
+    pass
+
+class RV(RecycleView):
+    def __init__(self, **kwargs):
+        super(RV, self).__init__(**kwargs)
+        global totaltags, response, final, sortedtags, distrib
+
+        self.data = [{'text': totaltags(x)} for x in range(10)]
 
 class ScreenManagement(ScreenManager):
     def scan(self): # Disconnect RFID
-            global tags, response, final
+            global totaltags, response, final, sortedtags, distrib
             def regs(list):
                 regex_string = re.findall(r'(?<=01)30\d{22}(?=b)' , list)
                 return regex_string
@@ -68,13 +123,17 @@ class ScreenManagement(ScreenManager):
                 response = binascii.hexlify(ser.readline())
                 response = str(response, 'UTF8')
                 reduc = regs(response)
-                tags = list(set(tags).union(set(reduc)))
+                totaltags.extend(reduc)
+                totaltags.sort()
+                sortedtags = list(set(totaltags).union(set(reduc)))
+                sortedtags.sort()
+                distrib = [len(list(group)) for key, group in groupby(totaltags)]
             print('\n\n')
-            tags.sort()
-            print(tags)
-            print(len(tags))
+            print(totaltags)
+            print(sortedtags)
+            print(len(sortedtags))
+            print(len(distrib))
     pass
-
 
 presentation = Builder.load_file("kiocery.kv")
 
--- sample.py
+++ sample.py
@@ -1,54 +1,124 @@
-import kivy
-
-
 from kivy.app import App
-from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from kivy.properties import BooleanProperty
+from kivy.properties import NumericProperty
+from kivy.uix.behaviors import FocusBehavior
 from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.properties import ObjectProperty
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class ScreenOne(Screen):
+Builder.load_string('''
+#:import random random.random
 
-    def __init__ (self,**kwargs):
-        super (ScreenOne, self).__init__(**kwargs)
+<CustomScreen>:
+    hue: random()
+    canvas:
+        Color:
+            hsv: self.hue, .5, .3
+        Rectangle:
+            size: self.size
 
-        my_box1 = BoxLayout(orientation='vertical')
-        my_label1 = Label(text="BlaBlaBla on screen 1", font_size='24dp')
-        my_button1 = Button(text="Go to screen 2",size_hint_y=None, size_y=100)
-        my_button1.bind(on_press=self.changer)
-        my_box1.add_widget(my_label1)
-        my_box1.add_widget(my_button1)
-        self.add_widget(my_box1)
+    Label:
+        font_size: 42
+        text: root.name
 
-    def changer(self,*args):
-        self.manager.current = 'screen2'
+    Button:
+        text: 'Next screen'
+        size_hint: None, None
+        pos_hint: {'right': 1}
+        size: 150, 50
+        on_release: root.manager.current = root.manager.next()
 
-class ScreenTwo(Screen):
+<SelectableLabel>:
+    # Draw a background to indicate selection
+    canvas.before:
+        Color:
+            rgba: (.0, 0.9, .1, .3) if self.selected else (0, 0, 0, 1)
+        Rectangle:
+            pos: self.pos
+            size: self.size
+<RV>:
+    viewclass: 'SelectableLabel'
+    SelectableRecycleBoxLayout:
+        default_size: None, dp(56)
+        default_size_hint: 1, None
+        size_hint_y: None
+        height: self.minimum_height
+        orientation: 'vertical'
+        multiselect: True
+        touch_multiselect: True
 
-    def __init__(self,**kwargs):
-        super (ScreenTwo,self).__init__(**kwargs)
+<RVScreen>:
+    BoxLayout:
+        orientation: "vertical"
+        RV:
+        Button:
+            text: 'Previous screen'
+            size_hint: None, None
+            size: 150, 50
+            on_release: root.manager.current = root.manager.previous()
 
-        my_box1 = BoxLayout(orientation='vertical')
-        my_label1 = Label(text="BlaBlaBla on screen 2",font_size='24dp')
-        my_button1 = Button(text="Go to screen 1",size_hint_y=None, size_y=100)
-        my_button1.bind(on_press=self.changer)
-        my_box1.add_widget(my_label1)
-        my_box1.add_widget(my_button1)
-        self.add_widget(my_box1)
+''')
 
-    def changer(self,*args):
-        self.manager.current = 'screen1'
 
-class TestApp(App):
+class CustomScreen(Screen):
+    hue = NumericProperty(0)
 
-        def build(self):
-            my_screenmanager = ScreenManager()
-            screen1 = ScreenOne(name='screen1')
-            screen2 = ScreenTwo(name='screen2')
-            my_screenmanager.add_widget(screen1)
-            my_screenmanager.add_widget(screen2)
-            return my_screenmanager
+
+class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
+                                 RecycleBoxLayout):
+    ''' Adds selection and focus behaviour to the view. '''
+
+
+class SelectableLabel(RecycleDataViewBehavior, Label):
+    ''' Add selection support to the Label '''
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        ''' Catch and handle the view changes '''
+        self.index = index
+        return super(SelectableLabel, self).refresh_view_attrs(
+            rv, index, data)
+
+    def on_touch_down(self, touch):
+        ''' Add selection on touch down '''
+        if super(SelectableLabel, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        ''' Respond to the selection of items in the view. '''
+        self.selected = is_selected
+        if is_selected:
+            print("selection changed to {0}".format(rv.data[index]))
+        else:
+            print("selection removed for {0}".format(rv.data[index]))
+
+
+class RV(RecycleView):
+    def __init__(self, **kwargs):
+        super(RV, self).__init__(**kwargs)
+        self.data = [{'text': str(x)} for x in range(10)]
+
+
+class RVScreen(Screen):
+    pass
+
+
+class ScreenManagerApp(App):
+
+    def build(self):
+        root = ScreenManager()
+        root.add_widget(CustomScreen(name='CustomScreen'))
+        root.add_widget(RVScreen(name='RVScreen'))
+        return root
+
 
 if __name__ == '__main__':
-    TestApp().run()
+    ScreenManagerApp().run()
